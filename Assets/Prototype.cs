2198d224b54a10756635ea4bea613f55a47f7457
moayadrayyan <moayad_rayyan@outlook.com>
2020-11-13 16:45:39 +0400

    question 5 solution

---
+++ Assets/Prototype.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Prototype : MonoBehaviour
+{
+    public string Paragraph = "";
+    public AudioSource BeepClip;
+
+    private Queue WordsStack = new Queue();
+
+    IEnumerator Start()
+    {
+        BeepClip.playOnAwake = false;
+        WordsStack = new Queue(Paragraph.Split(' '));
+        while (WordsStack.Count > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            PrintWords();
+        }
+    }
+
+    void PrintWords()
+    {
+        if (WordsStack.Count > 0)
+        {
+            gameObject.transform.Find("Panel").transform.Find("WordsArea").GetComponent<Text>().text += " " + WordsStack.Dequeue();
+            BeepClip.Play();
+        }
+    }
+}
